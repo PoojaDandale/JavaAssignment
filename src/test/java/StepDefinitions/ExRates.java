0f37bfb23c1a8db99122d7830c3141d3ba7263a0
pooja.dandale <pooja.dandale@PUNITP357880L.ad.infosys.com>
2020-10-22 21:35:04 +0530

    committed

--- src/test/java/StepDefinitions/ExRates.java
+++ src/test/java/StepDefinitions/ExRates.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.When;
 import static io.restassured.RestAssured.*;
 import org.testng.Assert;
 import io.restassured.response.Response;
-
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class ExRates{
 	
@@ -26,5 +26,57 @@ public class ExRates{
 	    Assert.assertEquals(200, r.getStatusCode());
 	    System.out.println("Status Code="+r.getStatusCode());
 	}
+	
+	@Then("Validate the response")
+	public void validate_the_response() {
+	    r.then().body("base", equalTo("EUR"));
+	    System.out.println("Body validation");
+	}
+
+	@When("An incorrect or incomplete url is provided")
+	public void an_incorrect_or_incomplete_url_is_provided() {
+	    r=given().get("https://api.ratesapi.io/api/");
+	}
+
+	@Then("Validate the correct response")
+	public void validate_the_correct_response() {
+	    System.out.println("Incorrect url");
+	    r.then().body("error",equalTo("time data 'api' does not match format '%Y-%m-%d'"));
+	}
+	
+	Response res;
+	
+	@Given("Rates API for specific date Foreign Exchange rates")
+	public void rates_API_for_specific_date_Foreign_Exchange_rates() {
+	    System.out.println("API launch for foreign rates");
+	}
+
+	@When("The API is available for foreign")
+	public void the_API_is_available_for_foreign() {
+	   res = given().get("https://api.ratesapi.io/api/2020-10-22");
+	}
+
+	@Then("success status")
+	public void success_status() {
+		System.out.println("Status code"+res.getStatusCode());
+		Assert.assertEquals(200,res.getStatusCode());
+	    	}
+
+	
+	@Then("response assertion")
+	public void response_assertion() {
+	    res.then().body("base",equalTo("EUR"));	}
+
+	
+	@When("The future date is privided in the url")
+	public void the_future_date_is_privided_in_the_url() {
+		res=given().get("https://api.ratesapi.io/api/2020-12-15");
+	    	}
+
+	@Then("future validation")
+	public void future_validation() {
+	    res.then().body("date",equalTo("2020-10-08"));
+	}
+
 
 }
--- src/test/resources/Features/ForeignExRates.feature
+++ src/test/resources/Features/ForeignExRates.feature
@@ -2,19 +2,30 @@ Feature: Foreign currency exchange rates api with currency conversion
 
   Scenario: Checking status code
     Given Rates API for Latest Foreign Exchange rates
-    When  The API is available
-    Then  Validate the code
-    
+    When The API is available
+    Then Validate the code
+
   Scenario: Checking response
     Given Rates API for Latest Foreign Exchange rates
-    When  The API is available
-    Then  Validate the response
-    
+    When The API is available
+    Then Validate the response
+
   Scenario: Checking error response
     Given Rates API for Latest Foreign Exchange rates
-    When  An incorrect or incomplete url is provided
-    Then  Validate the correct response
-    
-  
+    When An incorrect or incomplete url is provided
+    Then Validate the correct response
 
- 
+  Scenario: Foreign Exchange rates
+    Given Rates API for specific date Foreign Exchange rates
+    When The API is available for foreign
+    Then success status
+
+  Scenario: Foreign Exchange rates latest
+    Given Rates API for specific date Foreign Exchange
+    When The API is available for foreign
+    Then response assertion
+
+  Scenario: Foreign Exchange rates of today
+    Given Rates API for specific date Foreign Exchange rate
+    When The future date is privided in the url
+    Then future validation
